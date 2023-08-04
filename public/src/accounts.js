b0b008da418f4af7f4f22548b32c5dae596c613b
EBassettHound <ebass227@gmail.com>
2023-08-04 11:59:18 -0400

    Complete qualified assignment, ...

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -3,6 +3,10 @@ let booksArr = require("../data/books");
 let authorsArr = require("../data/authors");
 let partition = require("./books").partitionBooksByBorrowedStatus;
 
+// HELPER FUNCTION **************************
+const getPartition = booksArr => partition(booksArr)
+// HELPER FUNCTION **************************
+
 const findAccountById = (accountsArr, id) => accountsArr.find(accObj=>accObj.id === id);
 const sortAccountsByLastName = (accountsArr) => accountsArr.sort((accA,accB)=> (accA.name.last) < (accB.name.last) ? -1 : 1);
 const getTotalNumberOfBorrows = ({id}, booksArr) =>{
@@ -12,7 +16,9 @@ const getTotalNumberOfBorrows = ({id}, booksArr) =>{
 }
 
 function getBooksPossessedByAccount(account, booksArr, authorsArr) {
-  const [checkedOut] = partition(booksArr);
+  // EMPLOYMENT OF HELPER FUNCTION *****************************************************
+  const [checkedOut] = getPartition(booksArr);
+  // EMPLOYMENT OF HELPER FUNCTION *****************************************************
   let result = checkedOut.filter(bookObj=> { if (bookObj.borrows[0].id === account.id) { 
     authorsArr.forEach(authorObj => {if (authorObj.id === bookObj.authorId)bookObj.author = authorObj});
     return true;
